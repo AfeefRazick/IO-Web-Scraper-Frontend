3615a0473c7697bbfbfec3c1860f3400a7e2baca
AfeefRazick <afeefrazickamir@gmail.com>
2023-09-12 15:24:44 +0530

    setup tailwind and concurrent build watch

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    // "./*.{html,jsx}",
+    "./src/*.{html,jsx}",
+    "./src/**/*.{html,jsx}",
+    "./src/**/**/*.{html,jsx}",
+    "./src/**/**/**/*.{html,jsx}",
+  ],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
